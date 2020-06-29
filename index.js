973b78f279e5877c95fc91c49f72044756df9291
rjhern37 <rjhern37@neiu.edu>
2020-06-29 13:27:33 -0500

    added local storage save and delete functinality

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//var localStorage = 
-//var  = 'this is some new text'
+
 
 var saveBtn = $('.saveBtn');
 var deleteBtn = $('.deleteBtn');
@@ -11,7 +10,7 @@ var timeBlocks = $(".timeBlock")
 currentDayEl.text(moment().format("MMMM Do YYYY, hh:mm:ss a"));
 
 
-//Function to change color of hours that have passed   
+//Function to display current day/Time on Jumbotron
 var updateTime = function (){
     currentDayEl.text(moment().format("MMMM Do YYYY, hh:mm:ss a"));
 
@@ -26,26 +25,23 @@ var updateTime = function (){
         }
     });
 }
-
 setInterval(updateTime, 1000)
 
 
 // Function to add event info to local storage && to show a 'Event successfully saved' alert
 function save(){
-    var eventText = ""
-    
     saveBtn.on('click', function(){
-        var events = $('#eventInfo').val();
-        console.log(localStorage)
-        localStorage.setItem('eventInfo', events)
-        console.log(events) 
+        var btnData = $(this).attr("saveTime");
+        console.log(btnData)
+
+        var descriptionTarget = $("#eventInfo" + btnData);
+        console.log(descriptionTarget);
+        console.log(descriptionTarget.val());
+
+        localStorage.setItem(descriptionTarget.attr("id"), descriptionTarget.val())
         alert("Saved to your calender!") 
     });
-
-
-
 };
-
 save();
 
 
@@ -55,8 +51,14 @@ function reset(){
 
 
     deleteBtn.on('click', function(){
-            localStorage.removeItem('eventInfo', events)
-            alert('you clicked a delete button')
+        var btnData = $(this).attr("delTime");
+        console.log(btnData)
+
+        var descriptionTarget = $("#eventInfo" + btnData);
+        localStorage.removeItem(descriptionTarget.attr("id"), descriptionTarget.val());
+        // $(".description").text("")
+        descriptionTarget.attr("id").text("");
+        alert('You deleted an event.')
         });
     
 
@@ -65,12 +67,23 @@ reset();
 
 
 //Function to display current day/Time on Jumbotron
-var currentDay;
-
-function currentTime() {
 
 
-};
 
 
+
+
+
+$("#eventInfo7").text(localStorage.getItem("eventInfo7"));
+$("#eventInfo8").text(localStorage.getItem("eventInfo8"));
+$("#eventInfo9").text(localStorage.getItem("eventInfo9"));
+$("#eventInfo10").text(localStorage.getItem("eventInfo10"));
+$("#eventInfo11").text(localStorage.getItem("eventInfo11"));
+$("#eventInfo12").text(localStorage.getItem("eventInfo12"));
+$("#eventInfo13").text(localStorage.getItem("eventInfo13"));
+$("#eventInfo14").text(localStorage.getItem("eventInfo14"));
+$("#eventInfo15").text(localStorage.getItem("eventInfo15"));
+$("#eventInfo16").text(localStorage.getItem("eventInfo16"));
+$("#eventInfo17").text(localStorage.getItem("eventInfo17"));
+$("#eventInfo18").text(localStorage.getItem("eventInfo18"));
 
