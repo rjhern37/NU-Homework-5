69176c3573d72486bf622b9708a10021026ddbb4
rjhern37 <rjhern37@neiu.edu>
2020-06-29 12:26:07 -0500

    added live time to jumbotron

--- index.js
+++ index.js
@@ -6,7 +6,28 @@ var deleteBtn = $('.deleteBtn');
 
 var currentDayEl = $("#currentDay");
 
-currentDayEl.text(moment().format("MMMM Do YYYY, hh:mm:ss a"))
+var timeBlocks = $(".timeBlock")
+
+currentDayEl.text(moment().format("MMMM Do YYYY, hh:mm:ss a"));
+
+
+//Function to change color of hours that have passed   
+var updateTime = function (){
+    currentDayEl.text(moment().format("MMMM Do YYYY, hh:mm:ss a"));
+
+    var currentHour = moment().hour();
+
+    timeBlocks.each(function (i, element){
+        element = $(element);
+        if(currentHour > element.attr("data-hour")){
+            element.addClass("past").removeClass("future present");
+        } else if (currentHour == element.attr("data-hour")){
+            element.addClass("present").removeClass("future past");
+        }
+    });
+}
+
+setInterval(updateTime, 1000)
 
 
 // Function to add event info to local storage && to show a 'Event successfully saved' alert
@@ -17,8 +38,8 @@ function save(){
         var events = $('#eventInfo').val();
         console.log(localStorage)
         localStorage.setItem('eventInfo', events)
-        console.log(localStorage) 
-        alert(localStorage.getItem('name')) 
+        console.log(events) 
+        alert("Saved to your calender!") 
     });
 
 
@@ -30,32 +51,17 @@ save();
 
 //Function to delete (reset) events that were previously saved to local storage && to show a 'Event successfully deleted' alert
 function reset(){
-    var events = $('#eventInfo').val();
+    // var events = $('#eventInfo').val();
 
-        localStorage.setItem('eventInfo', events);
-        deleteBtn.on('click', function(){
+
+    deleteBtn.on('click', function(){
+            localStorage.removeItem('eventInfo', events)
             alert('you clicked a delete button')
         });
     
-    
-    
-
-
 
 };
 reset();
-
-//Function to change color of hours that have passed 
-
-function timePassed(){
-    
-    var colors;
-    var hours;
-
-    var moment = require ("moment");
-
-
-};
 
 
 //Function to display current day/Time on Jumbotron
